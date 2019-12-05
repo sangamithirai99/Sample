4604da0d4c7a4d16ac294f62dd627eaa4e1dc7c8
Thara Viswanathan <tharabrindharani@gmail.com>
2019-12-05 13:18:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function showName(name) {
+    console.log(name)
+}
+document.querySelector("#timebutton").addEventListener("click", function(){
+    document.getElementById("timebutton").innerHTML = "xxxx";
+  });
+
+showName('Sangu')
